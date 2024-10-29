f11e13c284e2b1d26ff8414c911e62548f68dd55
Oscar Virot <virot@virot.com>
2024-10-29 18:40:18 +0100

    Update the logging to prepair for advancedLogging

--- TameMyCerts/Models/AdvancedPolicy.cs
+++ TameMyCerts/Models/AdvancedPolicy.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
 
 namespace TameMyCerts.Models
 {
@@ -16,5 +21,57 @@ namespace TameMyCerts.Models
 
         [XmlElement(ElementName = "AdvancedADPolicy")]
         public List<AdvancedADPolicy> AdvancedADPolicy { get; set; } = new List<AdvancedADPolicy>();
+
+        private static string ConvertToHumanReadableXml(string inputString)
+        {
+            var xmlWriterSettings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = true,
+                Indent = true,
+                NewLineOnAttributes = true
+            };
+
+            var stringBuilder = new StringBuilder();
+
+            var xElement = XElement.Parse(inputString);
+
+            using (var xmlWriter = XmlWriter.Create(stringBuilder, xmlWriterSettings))
+            {
+                xElement.Save(xmlWriter);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public void SaveToFile(string path)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(AdvancedPolicy));
+
+            using (var stringWriter = new StringWriter())
+            {
+                using (var xmlWriter = XmlWriter.Create(stringWriter))
+                {
+                    xmlSerializer.Serialize(xmlWriter, this);
+                    var xmlData = stringWriter.ToString();
+
+                    File.WriteAllText(path, ConvertToHumanReadableXml(xmlData));
+                }
+            }
+        }
+        public string SaveToString()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(AdvancedPolicy));
+
+            using (var stringWriter = new StringWriter())
+            {
+                using (var xmlWriter = XmlWriter.Create(stringWriter))
+                {
+                    xmlSerializer.Serialize(xmlWriter, this);
+                    var xmlData = stringWriter.ToString();
+
+                    return ConvertToHumanReadableXml(xmlData);
+                }
+            }
+        }
     }
 }
--- TameMyCerts/Validators/AdvancedPoliciesValidator.cs
+++ TameMyCerts/Validators/AdvancedPoliciesValidator.cs
@@ -40,33 +40,62 @@ namespace TameMyCerts.Validators
         {
             foreach (var policy in policies)
             {
+                AdvancedPolicy matchedPolicy = new AdvancedPolicy();
+                matchedPolicy.PolicyName = policy.PolicyName;
+                matchedPolicy.Action = policy.Action;
                 bool policyMatch = true;
                 // Check if the policy is a match for the AdvancedADPolicy
                 Console.WriteLine("Checking AdvancedADPolicy");
-                // If there are any AdvancedADPolicies, then check if any of them are a match, else go to next policy
-                if (policy.AdvancedADPolicy.Any(adp => _advancedADPoliciesValidator.FullfillsPolicy(adp, dsObject)))
+                if (policy.AdvancedADPolicy.Any())
                 {
-                    // We have a match, check any other Policies before setting the result
-                    Console.WriteLine("Matched one AD Policy");
-                }
-                else
-                {
-                    Console.WriteLine("Matched no AD Policy");
-                    policyMatch = false;
+                    bool adpMatch = false;
+                    // If there are any AdvancedADPolicies, then check if any of them are a match, else go to next policy
+                    foreach (var adp in policy.AdvancedADPolicy)
+                    {
+                        if (_advancedADPoliciesValidator.FullfillsPolicy(adp, dsObject))
+                        {
+                            // We have a match, 
+                            Console.WriteLine("Matched one AD Policy");
+                            matchedPolicy.AdvancedADPolicy.Add(adp);
+                            adpMatch = true;
+                            break;
+                        }
+                        // If we get here, then no advancedADPolicy matched, so we can set the policyMatch to false
+                    }
+                    if (adpMatch == false)
+                    {
+                        Console.WriteLine("Matched no AD Policy");
+                        policyMatch = false;
+                    }
                 }
                 // Check for match with a AdvancedCertificateRequestPolicy
                 // Same logic as above but for the CertificateRequestPolicy
-                
+
                 // Check if the policy is a match for the AdvancedYubikeyPolicy
                 // Same logic as above but for the Yubikey
 
-                if (policy.Action == "Issue" && policyMatch == true ||
-                    policy.Action == "IssueOnFail" && policyMatch != false)
+                if (policy.Action == "Issue" && policyMatch == true)
                 {
+                    // Log to the operational log what policy was matched
+                    Console.WriteLine(matchedPolicy.SaveToString());
                     return result;
                 }
-                else 
+                else if (policy.Action == "IssueOnFail" && policyMatch != false)
                 {
+                    // Log to the operational log what policy failed to matched
+                    return result;
+                }
+                else if (policy.Action == "Reject" && policyMatch == true)
+                {
+                    // Log to the operational log what policy was matched
+                    result.SetFailureStatus(WinError.CERTSRV_E_TEMPLATE_DENIED, string.Format(
+                        "Failed on policy: {0}",
+                        policy.PolicyName));
+                    return result;
+                }
+                else if (policy.Action == "RejectOnFail" && policyMatch == false)
+                {
+                    // Log to the operational log what policy failed to matched
                     result.SetFailureStatus(WinError.CERTSRV_E_TEMPLATE_DENIED, string.Format(
                         "Failed on policy: {0}",
                         policy.PolicyName));
